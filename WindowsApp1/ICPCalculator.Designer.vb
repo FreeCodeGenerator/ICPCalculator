e788888d0646283a3899fd35e8e99f6606d585d2
rluis <rluis@LAPTOP-R2TVPHR3>
2023-10-30 10:29:46 -0500

    icono nuevo

--- WindowsApp1/ICPCalculator.Designer.vb
+++ WindowsApp1/ICPCalculator.Designer.vb
@@ -79,9 +79,10 @@ Partial Class ICPCalculator
         'Label1
         '
         Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(301, 24)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(74, 13)
+        Me.Label1.Size = New System.Drawing.Size(237, 37)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "ICP Calculator"
         '
